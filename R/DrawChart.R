2efea9758313112e5b3b739b44e062ee20c94cf1
ItBlock <wnsuddlsla@gmail.com>
2018-11-28 19:21:36 +0900

    Update DrawChart.R

--- R/DrawChart.R
+++ R/DrawChart.R
@@ -7,6 +7,14 @@ source("gemini.R")
 #Library for Pie 3D function
 options(scipen = 999999)
 #Draw graph in one bar chart
+dir.create(file.path(getwd(), "../images"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "../images/Whole"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "../images/Person"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "../images/Death"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "../images/Visit"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "../images/Condition"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "../images/Drug exposure"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "../images/Drug era"), showWarnings = FALSE)
 ################################################################################
 #WHOLE TABLE info VISUALLIZATION
 ################################################################################
