f6635c3cfd9f1106aebe05f5baa2a5c34cc48833
ItBlock <wnsuddlsla@gmail.com>
2018-12-03 11:47:19 +0900

    Add code about assign error

--- R/GetData.R
+++ R/GetData.R
@@ -26,13 +26,13 @@ no_files <- setdiff(str_name_list,tar_name_list)
 ################################################################################
 if(length(no_files) != 0&&length(name_list)!=0){
     set_val(name_list)
-    lapply(no_files, function(x) assign(paste0("std_",gsub(x,pattern =".rds",replacement = "")),value = readRDS(paste0("../Standard RDS/", x)),envir=.GlobalEnv))
-    lapply(no_files, function(x) assign(paste0("tar_",gsub(x,pattern =".rds",replacement = "")),value = NULL,envir=.GlobalEnv))
+    lapply(no_files, function(x) assign(paste0("std_",gsub(x,pattern ="*.rds",replacement = "")),value = readRDS(paste0("../Standard RDS/", x)),envir=.GlobalEnv))
+    lapply(no_files, function(x) assign(paste0("tar_",gsub(x,pattern ="*.rds",replacement = "")),value = NULL,envir=.GlobalEnv))
 }else if(length(no_files) != 0&&length(name_list)==0){
     pms <- readline('No RDS file in Target RDS folder. Do you want to proceed(y / n)? ')
     if(pms == "y" || pms == "Y"){
-        lapply(no_files, function(x) assign(paste0("std_",gsub(x,pattern =".rds",replacement = "")),value = readRDS(paste0("../Standard RDS/", x)),envir=.GlobalEnv))
-        lapply(no_files, function(x) assign(paste0("tar_",gsub(x,pattern =".rds",replacement = "")),value = readRDS(paste0("../Standard RDS/", x)),envir=.GlobalEnv))
+        lapply(no_files, function(x) assign(paste0("std_",gsub(x,pattern ="*.rds",replacement = "")),value = readRDS(paste0("../Standard RDS/", x)),envir=.GlobalEnv))
+        lapply(no_files, function(x) assign(paste0("tar_",gsub(x,pattern ="*.rds",replacement = "")),value = readRDS(paste0("../Standard RDS/", x)),envir=.GlobalEnv))
     }else if (pms == "n" || pms == "N"){
         message("Proceed is stopped.")
     }else{
