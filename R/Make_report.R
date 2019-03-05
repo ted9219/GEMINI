e1ce900a0eeb5c10679b3311f390d1df2eeb8ec2
ted <ted9219@gmail.com>
2019-03-05 10:15:59 +0900

    add korean

--- R/Make_report.R
+++ R/Make_report.R
@@ -8,9 +8,12 @@
 # Execute R markdown file
 make_report<-function(){
     tryCatch({
-        file.copy(from = paste0(.libPaths()[1],"/gemini/data/Gemini_md.Rmd"), to = getwd(),overwrite = T)
-        rmarkdown::render(paste0(getwd(),"/Gemini_md.Rmd"),encoding = "UTF-8")
-        browseURL(url=paste0(getwd(),"/Gemini_md.html"))
+        file.copy(from = paste0(.libPaths()[1],"/gemini/data/Gemini_md_eng.Rmd"), to = getwd(),overwrite = T)
+        rmarkdown::render(paste0(getwd(),"/Gemini_md_eng.Rmd"),encoding = "UTF-8")
+        browseURL(url=paste0(getwd(),"/Gemini_md_eng.html"))
+        file.copy(from = paste0(.libPaths()[1],"/gemini/data/Gemini_md_kor.Rmd"), to = getwd(),overwrite = T)
+        rmarkdown::render(paste0(getwd(),"/Gemini_md_kor.Rmd"),encoding = "UTF-8")
+        browseURL(url=paste0(getwd(),"/Gemini_md_kor.html"))
     },error = function(x){
         message("Need Rmd file.")
     })
